846acb758ec61092da66aab4e3c6e9e605b5e5ed
Raman Sahi <sahiraman7@gmail.com>
2024-08-12 12:40:11 +0530

    made changes to database.js and passport.js file

--- config/database.js
+++ config/database.js
@@ -1,15 +1,42 @@
+// for Compass
+
+// require library
+// const mongoose = require("mongoose");
+
+// // connect to the DB
+// mongoose.connect("mongodb://localhost/hospitalAPI");
+
+// // check if DB is connected or Not
+// const db = mongoose.connection;
+
+// db.on(
+//   "error",
+//   console.error.bind(console, "Error in connecting to the DataBase")
+// );
+
+// db.once("open", function () {
+//   console.log("DataBase connected Successfully");
+// });
+
+// module.exports = db;
+
+// for atlas
+
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://sahiraman7:Y8oBMVfbG6YlPUzj@cluster0.dncnotl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0y"
-);
+// Replace with your actual MongoDB connection string
+const mongoURI =
+  "mongodb+srv://sahiraman7:msUVnekqI4wTLyQ2@cluster0.lothw.mongodb.net/HospitalAPI?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Successfully Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error in connecting to MongoDB:", err.message);
+  });
 
 const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "Error in connecting with mongoDB"));
-
-db.once("open", () => {
-  console.log("Successfully Connected with mongoDB");
-});
 
 module.exports = db;
