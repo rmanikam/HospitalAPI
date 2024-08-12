774475cc50310311ee22eb77b9cc23d76171c981
Raman Sahi <sahiraman7@gmail.com>
2024-08-12 11:34:12 +0530

    made changes to passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,8 @@ const Doctor = require("../models/doctor");
 
 const passport = require("passport");
 
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = "secret";
